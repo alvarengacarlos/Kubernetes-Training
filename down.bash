4957fa7e9945cab35cac7a2be8a59207081a658b
alvarengacarlos <carlosho200@hotmail.com>
2025-11-01 13:38:40 -0400

    Removes api and db services

--- down.bash
+++ down.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 MK_DOCS_IMG_TAG="doc:v1.0.0"
-MONGO_DB_IMG_TAG="mongodb:v1.0.0"
-SPRING_BOOT_IMG_TAG="api:v1.0.0"
 JOB_IMG_TAG="job:v1.0.0"
 
 destroy_img_fn() {
@@ -12,6 +10,4 @@ destroy_img_fn() {
 eval $(minikube docker-env)
 
 destroy_img_fn $MK_DOCS_IMG_TAG
-destroy_img_fn $MONGO_DB_IMG_TAG
-destroy_img_fn $SPRING_BOOT_IMG_TAG
 destroy_img_fn $JOB_IMG_TAG
--- up.bash
+++ up.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 MK_DOCS_IMG_TAG="doc:v1.0.0"
-MONGO_DB_IMG_TAG="mongodb:v1.0.0"
-SPRING_BOOT_IMG_TAG="api:v1.0.0"
 JOB_IMG_TAG="job:v1.0.0"
 
 create_img_fn() {
@@ -12,8 +10,6 @@ create_img_fn() {
 eval $(minikube docker-env)
 
 create_img_fn "doc-service/Dockerfile" $MK_DOCS_IMG_TAG "doc-service"
-create_img_fn "api-service/Dockerfile" $SPRING_BOOT_IMG_TAG "api-service"
-create_img_fn "db-service/Dockerfile" $MONGO_DB_IMG_TAG "db-service"
 create_img_fn "job-service/Dockerfile" $JOB_IMG_TAG "job-service"
 
 echo "Ready"
